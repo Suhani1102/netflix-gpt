39a3a14d87e0b770a6a1952ce5496d782ef7a78f
Suhani1102 <sandhusuhani1102@gmail.com>
2025-05-22 23:36:55 +0530

    Display Movie Trailer

---
+++ src/components/VideoTitle.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const VideoTitle = ({ title, overview }) => {
+  return (
+    <div className="w-screen aspect-video pt-[20%] px-12 absolute text-white bg-gradient-to-r from-zinc-800 to-50%">
+      <h1 className="text-3xl font-bold">{title}</h1>
+      <p className="py-6 text-md w-1/4">{overview}</p>
+      <div>
+        <button className="w-auto bg-white text-black text-lg py-2 px-8 w-10  rounded-lg">
+          Play
+        </button>
+        <button className="w-auto mx-2 bg-gray-500 text-white text-lg py-2 px-8 w-10 bg-opacity-50 rounded-lg">
+          More Info
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default VideoTitle;
